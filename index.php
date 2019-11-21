ec5b9b0d7869a9573bfe3f83c8ef424fad38cdfa
Tomoaki imac5k-2017 <tomoaki.nagahara@gmail.com>
2019-11-21 13:10:02 +0900

    New: index.php

---
+++ index.php
@@ -0,0 +1,12 @@
+<?php
+/**
+ * unit-template:/index.php
+ *
+ * @creation  2019-02-22
+ * @version   1.0
+ * @package   unit-template
+ * @author    Tomoaki Nagahara <tomoaki.nagahara@gmail.com>
+ * @copyright Tomoaki Nagahara All right reserved.
+ */
+//	...
+include(__DIR__.'/Template.class.php');
